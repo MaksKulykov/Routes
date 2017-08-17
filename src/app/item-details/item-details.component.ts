9ba208079013d0796b5089806383ced3025b094a
MaksKulikov <kulykov.maks@gmail.com>
2017-08-17 13:31:13 +0300

    added button buy in item-details component

--- src/app/item-details/item-details.component.ts
+++ src/app/item-details/item-details.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 import { Item } from '../models/item';
 
@@ -11,18 +12,23 @@ import { Item } from '../models/item';
 })
 export class ItemDetailsComponent implements OnInit {
   title = 'Описание товара';
+  id: number;
   status: string;
   item: Item;
 
   constructor(private activatedRoute: ActivatedRoute,
-              private location: Location) {
+              private location: Location,
+              private router: Router) {
   }
 
   ngOnInit() {
     this.item = new Item();
-    this.activatedRoute.params.subscribe(params => {this.status = params['status']; });
+    this.activatedRoute.params.subscribe(params => {
+      this.status = params['status'];
+      this.id = +params['id'];
+      this.goToList();
+    });
     this.item = this.activatedRoute.snapshot.data['item'];
-    this.goToList();
   }
 
   goBack(): void {
@@ -31,9 +37,12 @@ export class ItemDetailsComponent implements OnInit {
   goToList(): void {
     if (this.status === 'buy') {
       setTimeout(() => {
-        this.goBack();
+        this.router.navigate(['/items']);
       }, 5000);
     }
+  }
+  goToBuy(): void {
+    this.router.navigate(['/item', this.id, 'buy']);
   }
 }
 
